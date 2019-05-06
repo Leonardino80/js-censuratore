136b0feb02bb2448f0e2b091176fb527c91909a6
Leonardino80 <gilgamesh2005@gmail.com>
2019-05-06 06:31:14 +0200

    finale incompleto

--- main.js
+++ main.js
@@ -3,40 +3,30 @@
 // - Stampa un “badword index” calcolato come il numero di parole censurate su il numero di parole totali
 
 // - Il software riceve in input un lungo testo e una serie di parole da censurare.
-var text = prompt ('inserisci il testo');
+var text = prompt ('inserisci il testo').split(/ /gi);
 var words;
 var input_phase = true;
 var censored_words = [];
 
-function check_stored_words (array, given_word){
-var got_word = false;
-  for (var i = 0; i < array.length; i++) {
-    if(array[i] === given_word) {
-      alert('word already inside')
-      var got_word = true;
-    }
-  }
-  return got_word;
-}
-
-
-
-console.log(really_got_word);
-
 while (input_phase ) {
   words = prompt ('inserisci parola vietata.');
-  check_stored_words (censored_words , words );
-  if (really_got_word = false) {
-      censored_words.push(words);
-  }
+  censored_words.push(words);
   input_phase = confirm ('vuoi inserire un altra parola?');
-
 }
 
-console.log(censored_words);
-var really_got_word = check_stored_words (censored_words , words );
-console.log(really_got_word);
+function censored (testo, censura) {
+  cens_index = 0;
+  for (var i = 0; i < testo.length; i++) {
+    if (censura.includes(testo[i]) == true) {
+      array1[i] = "xxx";
+      cens_index++;
+    }
+  }
+}
 
+var risultato = censored ( text , censored_words );
+text.join('');
+console.log(cens_index);
 // - Restituisce il testo con xxx al posto delle parole censurate.
 
 
